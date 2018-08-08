069a8c87d1da78f60a67f5445f4c835d820c4b3c
Neenus Gabriel <neenus@live.ca>
2018-08-08 21:19:17 +0000

    completed deleting URL assignment

--- express-server.js
+++ express-server.js
@@ -4,6 +4,18 @@ var PORT = 8080; // default port 8080
 
 app.set('view engine', 'ejs');
 
+// function to generate a six charecter random string 
+// this code snippet was copied from stackoverflow 
+function generateRandomString() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 6; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+}
+// console.log(generateRandomString());
+
 
 var urlDatabase = {
     "b2xVn2": "http://lighthouselabs.ca",
@@ -11,21 +23,34 @@ var urlDatabase = {
 };
 
 
-// views 
+// routes 
 // =========================================
 app.get("/urls", (req, res) => {
-    let templateVars = { urls: urlDatabase};
+    let templateVars = {
+        urls: urlDatabase
+    };
     res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+    // res.redirect("/urls");
+});
+
 app.get("/urls/:id", (req, res) => {
-    let templateVars = { 
+    let templateVars = {
         shortURL: req.params.id,
         longURL: urlDatabase[req.params.id]
     };
+    // console.log(shortURL);
     res.render("urls_show", templateVars);
-  });
+});
 
+app.post("/urls/:id/delete", (req, res) => {
+    console.log(urlDatabase[req.params.id]);
+    delete urlDatabase[req.params.id];
+    res.redirect("/urls");
+});
 // ===========================================
 
 
@@ -44,4 +69,21 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
     res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.post("/urls", (req, res) => {
+    // console.log(req.body); // debug statement to see POST parameters
+        let newid =  generateRandomString();
+        urlDatabase[newid] = req.body.longURL;
+    // };
+    // req.body.shortURL
+    // req.body.longURL
+    // res.send("Ok"); // Respond with 'Ok' (we will replace this)
+    res.redirect("/urls");
+
 });
